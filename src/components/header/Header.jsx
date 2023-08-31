e94f134f02e6adba6215b2938ac01c427411da5f
Mohamed Abdul-Wahab <mohamed.abdul1104@gmail.com>
2023-08-31 00:32:32 -0400

    Updated Header component, added social and asset components

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,9 +1,24 @@
 import React from 'react'
 import './header.css'
+import CTA from './CTA'
+import Selfie from '../../assets/selfie.jpg'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <header>
+      <div className="container header__container">
+        <h5>Hello I'm</h5>
+        <h1>Mohamed Abdul</h1>
+        <h5 className="text-light">Full Stack Developer</h5>
+        <CTA />
+
+        <div className="me">
+          <img src={Selfie} alt="" />
+        </div>
+
+        <a href="#contact" className='scroll__down'>Scroll Down</a>
+      </div>
+    </header>
   )
 }
 
---
+++ src/components/header/HeaderSocials.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const HeaderSocials = () => {
+  return (
+    <div className="header__socials">
+        <a href="https://linkedin.com/in/moeabdul" target="_blank"></a>
+        <a href="https://github.com/moe-abdul" target="_blank"></a>
+        <a href="https://instagram.com/moeinstaamoe" target="_blank"></a>
+    </div>
+  )
+}
+
+export default HeaderSocials
