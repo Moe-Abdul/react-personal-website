ba99134656d2961d497d515a932795ada92c3319
Mohamed Abdul-Wahab <mohamed.abdul1104@gmail.com>
2023-08-31 03:12:23 -0400

    Updated Nav component

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,9 +1,22 @@
 import React from 'react'
 import './about.css'
+import Selfie from '../../assets/selfieFull.png'
 
 const About = () => {
   return (
-    <div>About</div>
+    <section id='about'>
+      <h5>Get To Know</h5>
+      <h2>About me</h2>
+
+      <div className="container about__container">
+        <div className="about__me">
+          <div className="about__me-image">
+            <img src={Selfie} alt="" />
+          </div>
+        </div>
+        <div className="about__content"></div>
+      </div>
+    </section>
   )
 }
 
--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,9 +1,22 @@
 import React from 'react'
 import './nav.css'
+import {BiHomeAlt2} from 'react-icons/bi'
+import {AiOutlineInfoCircle} from 'react-icons/ai'
+import {BiBook} from 'react-icons/bi'
+import {LiaHandshake} from 'react-icons/lia'
+import {BiConversation} from 'react-icons/bi'
+import { useState } from 'react'
 
 const Nav = () => {
+  const [activeNav, setActiveNav] = useState('#')
   return (
-    <div>Nav</div>
+    <nav>
+      <a href="#home" title='Home' onClick={() => setActiveNav('#')} className={activeNav === '#' ? 'active' : ''}><BiHomeAlt2 /></a>
+      <a href="#about" title='About' onClick={() => setActiveNav('#about')} className={activeNav === '#about' ? 'active' : ''}><AiOutlineInfoCircle /></a>
+      <a href="#experience" title='Experience' onClick={() => setActiveNav('#experience')} className={activeNav === '#experience' ? 'active' : ''}><BiBook /></a>
+      <a href="#services" title='Services' onClick={() => setActiveNav('#services')} className={activeNav === '#services' ? 'active' : ''}><LiaHandshake /></a>
+      <a href="#contact" title='Contact' onClick={() => setActiveNav('#contact')} className={activeNav === '#contact' ? 'active' : ''}><BiConversation /></a>
+    </nav>
   )
 }
 
