487f89f59070625915b354369cce07b3d2ac165d
Mohamed Abdul-Wahab <114968177+Moe-Abdul@users.noreply.github.com>
2023-09-10 16:38:02 -0400

    Adding emailjs

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -4,8 +4,23 @@ import {HiOutlineMail} from 'react-icons/hi'
 import {FaPhoneAlt} from 'react-icons/fa'
 import {BsTwitter} from 'react-icons/bs'
 import {BsWhatsapp} from 'react-icons/bs'
+import React, { useRef } from 'react'
+import emailjs from 'emailjs-com'
 
 const Contact = () => {
+  const form  = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('YOUR_SERVICE_ID', 'YOUR_TEMPLATE_ID', form.current, 'YOUR_PUBLIC_KEY')
+      .then((result) => {
+          console.log(result.text);
+      }, (error) => {
+          console.log(error.text);
+      });
+  };
+
   return (
     <section id='contact'>
       <h5>Get In Touch</h5>
@@ -14,19 +29,19 @@ const Contact = () => {
       <div className="container contact__container">
         <div className="contact__options">
         <article className="contact__option">
-            <HiOutlineMail />
+            <HiOutlineMail className='contact__option-icon'/>
             <h4>Email</h4>
             <h5>mohamed_abdul@icloud.com</h5>
             <a href="mailto:mohamed_abdul@icloud.com">Send a message</a>
           </article>
           <article className="contact__option">
-            <FaPhoneAlt />
+            <FaPhoneAlt className='contact__option-icon'/>
             <h4>Phone</h4>
             <h5>(804) 243-1747</h5>
             <a href="tel:+18042431747">Give me a call</a>
           </article>
           <article className="contact__option">
-            <BsWhatsapp />
+            <BsWhatsapp className='contact__option-icon'/>
             <h4>WhatsApp</h4>
             <h5>(804) 243-1747</h5>
             <a href="https://api.whatsapp.com/send?phone=+18042431747" target="_blank">Send me a message</a>
