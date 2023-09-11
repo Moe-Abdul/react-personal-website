8890561cf005d2a0b1bd62991ad8f6c2b7ee4fe1
Mohamed Abdul-Wahab <mohamed.abdul1104@gmail.com>
2023-09-10 20:26:47 -0400

    Fixed navbar, updated footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,10 +1,48 @@
-import React from 'react'
-import './footer.css'
+import React from "react";
+import "./footer.css";
+import { BsInstagram } from "react-icons/bs";
+import { BsTwitter } from "react-icons/bs";
+import { BsDiscord } from "react-icons/bs";
 
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <footer>
+      <a href="#" className="footer__logo">
+        Moe
+      </a>
 
-export default Footer
+      <ul className="permalinks">
+        <li>
+          <a href="#">Home</a>
+        </li>
+        <li>
+          <a href="#about">About</a>
+        </li>
+        <li>
+          <a href="#experience">Experience</a>
+        </li>
+        <li>
+          <a href="#contact">Contact</a>
+        </li>
+      </ul>
+
+      <div className="footer__socials">
+        <a href="https://instagram.com">
+          <BsInstagram />
+        </a>
+        <a href="https://twitter.com">
+          <BsTwitter />
+        </a>
+        <a href="https://discord.com">
+          <BsDiscord />
+        </a>
+      </div>
+
+      <div className="footer__copyright">
+        <small>&copy; Mohamed Abdul Wahab. All rights reserved</small>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,23 +1,75 @@
-import React from 'react'
-import './nav.css'
-import {BiHomeAlt2} from 'react-icons/bi'
-import {AiOutlineInfoCircle} from 'react-icons/ai'
-import {BiBook} from 'react-icons/bi'
-import {LiaHandshake} from 'react-icons/lia'
-import {BiConversation} from 'react-icons/bi'
-import { useState } from 'react'
+import React, { useEffect, useState } from "react";
+import "./nav.css";
+import { BiHomeAlt2 } from "react-icons/bi";
+import { AiOutlineInfoCircle } from "react-icons/ai";
+import { BiBook } from "react-icons/bi";
+// import { LiaHandshake } from "react-icons/lia";
+import { BiConversation } from "react-icons/bi";
 
 const Nav = () => {
-  const [activeNav, setActiveNav] = useState('#')
+  const [activeNav, setActiveNav] = useState("#home");
+
+  const handleScroll = () => {
+    const sections = ["#home", "#about", "#experience", "#contact"];
+    let current;
+
+    for (const section of sections) {
+      const element = document.querySelector(section);
+      const rect = element.getBoundingClientRect();
+      if (rect.top <= window.innerHeight * 0.25 && rect.bottom >= 0) {
+        current = section;
+      }
+    }
+
+    if (current) {
+      setActiveNav(current);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
   return (
     <nav>
-      <a href="#home" title='Home' onClick={() => setActiveNav('#')} className={activeNav === '#' ? 'active' : ''}><BiHomeAlt2 /></a>
-      <a href="#about" title='About' onClick={() => setActiveNav('#about')} className={activeNav === '#about' ? 'active' : ''}><AiOutlineInfoCircle /></a>
-      <a href="#experience" title='Experience' onClick={() => setActiveNav('#experience')} className={activeNav === '#experience' ? 'active' : ''}><BiBook /></a>
-      <a href="#services" title='Services' onClick={() => setActiveNav('#services')} className={activeNav === '#services' ? 'active' : ''}><LiaHandshake /></a>
-      <a href="#contact" title='Contact' onClick={() => setActiveNav('#contact')} className={activeNav === '#contact' ? 'active' : ''}><BiConversation /></a>
+      <a
+        href="#home"
+        title="Home"
+        onClick={() => setActiveNav("#home")}
+        className={activeNav === "#home" ? "active" : ""}
+      >
+        <BiHomeAlt2 />
+      </a>
+      <a
+        href="#about"
+        title="About"
+        onClick={() => setActiveNav("#about")}
+        className={activeNav === "#about" ? "active" : ""}
+      >
+        <AiOutlineInfoCircle />
+      </a>
+      <a
+        href="#experience"
+        title="Experience"
+        onClick={() => setActiveNav("#experience")}
+        className={activeNav === "#experience" ? "active" : ""}
+      >
+        <BiBook />
+      </a>
+      {/* <a href="#services" title='Services' onClick={() => setActiveNav('#services')} className={activeNav === '#services' ? 'active' : ''}><LiaHandshake /></a> */}
+      <a
+        href="#contact"
+        title="Contact"
+        onClick={() => setActiveNav("#contact")}
+        className={activeNav === "#contact" ? "active" : ""}
+      >
+        <BiConversation />
+      </a>
     </nav>
-  )
-}
+  );
+};
 
-export default Nav
+export default Nav;
